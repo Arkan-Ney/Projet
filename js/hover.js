024cae019eb17dd9f4f224f11af4141fdce5fa0d
david toron <david@toron.popschool>
2017-03-21 11:23:13 +0100

    optimisation hover.js

--- js/hover.js
+++ js/hover.js
@@ -1,65 +1,19 @@
 $(document).ready(function(){
-	$("#actus").hover(function(){
-		$("#design-top").css("background-color", "#009839");
-		$(".login-btn").css("background-color", "#009839");
-		$(".navbar").css("background-color", "#009839");
-				$(".navbar").css("box-shadow", "0 0 4rem #009839 inset , 0 0 2rem black inset, 0 0 2rem black");
-	})
-})
+	function colorChange(lien, couleur, shadow){
+		$(lien).hover(function(){
+			$("#design-top").css("background-color", couleur);
+			$(".login-btn").css("background-color", couleur);
+			$(".navbar").css("background-color", couleur);
+			$(".navbar").css("box-shadow", shadow);
+		});
+	};
 
-$(document).ready(function(){
-	$("#contact").hover(function(){
-		$("#design-top").css("background-color", "#A8007F");
-		$(".login-btn").css("background-color", "#A8007F");
-		$(".navbar").css("background-color", "#A8007F");
-			$(".navbar").css("box-shadow", "0 0 4rem #A8007F inset , 0 0 2rem black inset, 0 0 2rem black");
-	})
-})
-
-$(document).ready(function(){
-	$("#classement").hover(function(){
-		$("#design-top").css("background-color", "#006D37");
-		$(".login-btn").css("background-color", "#006D37");
-		$(".navbar").css("background-color", "#006D37");
-			$(".navbar").css("box-shadow", "0 0 4rem #006D37 inset , 0 0 2rem black inset, 0 0 2rem black");
-	})
-})
-
-$(document).ready(function(){
-	$("#classement").hover(function(){
-		$("#design-top").css("background-color", "#006D37");
-		$(".login-btn").css("background-color", "#006D37");
-		$(".navbar").css("background-color", "#006D37");
-			$(".navbar").css("box-shadow", "0 0 4rem #006D37 inset , 0 0 2rem black inset, 0 0 2rem black");
-	})
-})
-
-$(document).ready(function(){
-	$("#basket-planet").hover(function(){
-		$("#design-top").css("background-color", "#740083");
-		$(".login-btn").css("background-color", "#740083");
-		$(".navbar").css("background-color", "#740083");
-			$(".navbar").css("box-shadow", "0 0 4rem #740083 inset , 0 0 2rem black inset, 0 0 2rem black");
-	})
-})
-
-$(document).ready(function(){
-	$("#calendrier").hover(function(){
-		$("#design-top").css("background-color", "#7FCD10");
-		$(".login-btn").css("background-color", "#7FCD10");
-		$(".navbar").css("background-color", "#7FCD10");
-			$(".navbar").css("box-shadow", "0 0 4rem #7FCD10 inset , 0 0 2rem black inset, 0 0 2rem black");
-	})
-})
-
-$(document).ready(function(){
-	$("#inscription").hover(function(){
-		$("#design-top").css("background-color", "#9445A0");
-		$(".login-btn").css("background-color", "#9445A0");
-		$(".navbar").css("background-color", "#9445A0");
-				$(".navbar").css("box-shadow", "0 0 4rem #9445A0 inset , 0 0 2rem black inset, 0 0 2rem black");
-	})
-})
-
-
+	colorChange ("#accueil", "#B5B1B3", "0 0 4rem #B5B1B3 inset , 0 0 2rem black inset, 0 0 2rem black");
+	colorChange ("#actus", "#009839", "0 0 4rem #009839 inset , 0 0 2rem black inset, 0 0 2rem black");
+	colorChange ("#contact", "#A8007F", "0 0 4rem #A8007F inset , 0 0 2rem black inset, 0 0 2rem black");
+	colorChange ("#classement", "#006D37", "0 0 4rem #006D37 inset , 0 0 2rem black inset, 0 0 2rem black");
+	colorChange ("#basket-planet", "#740083", "0 0 4rem #740083 inset , 0 0 2rem black inset, 0 0 2rem black");
+	colorChange ("#calendrier", "#7FCD10", "0 0 4rem #7FCD10 inset , 0 0 2rem black inset, 0 0 2rem black");
+	colorChange ("#inscription", "#9445A0", "0 0 4rem #9445A0 inset , 0 0 2rem black inset, 0 0 2rem black");
+});
 
